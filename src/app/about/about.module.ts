3c798ad0a134522c8b3c4205955e29599e8006d2
vpineda1996 <vpineda1996@gmail.com>
2019-04-30 11:30:21 -0700

    started with about page

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutHolderComponent } from './about-holder/about-holder.component';
 import { AboutRoutingModule } from './about-router.module';
+import { MatExpansionModule } from '@angular/material';
 
 @NgModule({
   declarations: [AboutHolderComponent],
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+
+    // Material utils
+    MatExpansionModule
   ]
 })
 export class AboutModule { }
